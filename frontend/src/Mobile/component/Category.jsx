7142ddabd5678b76446aca8e42ea4d12fb61809b
Durgesh_Titan <135988213+durgeshkapade@users.noreply.github.com>
2024-08-01 16:26:11 +0530

    Update Category.jsx
    update category data , and fetch from database

--- frontend/src/Mobile/component/Category.jsx
+++ frontend/src/Mobile/component/Category.jsx
@@ -6,6 +6,21 @@ import { useState } from 'react'
 export const Category = ({ item, func }) => {
     const [items, setItems] = useState([]);
 
+    useEffect(() => {
+        getPasswords();
+    }, [])
+
+    async function getPasswords() {
+        await axios.get('http://localhost:8080/')
+          .then((response) => {
+            setItems(response.data)
+    
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+    }
+
     return (
 
         <div className="px-4 py-1 shadow-md   gap-3 rounded-2xl bg-[#ffffff] text-nowrap scrollbar-thin mb-1 font-bold text-[#899198] hover:bg-[#0571ecdd] 
