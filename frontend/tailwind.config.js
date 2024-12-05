0400ae01a71d8d1be8b8410701038870361e1571
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-12-05 08:54:56 +0530

    tail config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcss_no_scrollbar from "tailwindcss-no-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,6 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-no-scrollbar')
+    tailwindcss_no_scrollbar
   ],
 }
