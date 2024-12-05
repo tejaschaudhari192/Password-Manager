d55cb84cf6d7927c34ac123d9195b2e1693d7504
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-12-05 09:37:14 +0530

    cors *

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,13 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-    credentials:true
-}))
+app.options('*', cors());
+
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+//     credentials:true
+// }))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/passwords', passwordRoutes);
