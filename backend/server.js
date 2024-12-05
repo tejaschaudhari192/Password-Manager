9494f6a7360f3d722aef8fc515f926810f3cdb2a
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-12-05 09:17:20 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin:['http://localhost:5000','https://password-manager-qaiv.vercel.app']
+}))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/passwords', passwordRoutes);
