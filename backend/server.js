f15e2501b6d9e75c4a17a9c17ac5b8d9c802b696
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-12-26 13:14:57 +0530

    *

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 const authRoutes = require('./src/routes/authRoutes');
-const passwordRoutes = require('./src/routes/passwordRoutes');
+const passwordsRoutes = require('./src/routes/passwordsRoutes');
+const passwordRoutes = require('./src/routes/passwordRoute');
 const cors = require('cors')
 const bodyParser = require("body-parser");
 require('dotenv').config();
@@ -11,16 +12,18 @@ connectDB();
 
 const app = express();
 
-
-
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors({
+    origin:['*']
+}));
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/passwords', passwordRoutes);
+app.use('/api/passwords', passwordsRoutes);
+app.use('/api/password', passwordRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
