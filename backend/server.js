8def219030607f9858c00d4ecf19ac3d2d29bf62
Tejas Chaudhari <jaybalaji192@gmail.com>
2025-01-03 10:12:36 +0530

    trying

--- backend/server.js
+++ backend/server.js
@@ -14,20 +14,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {  
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header(
-      'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
-    );
-    res.header('Access-Control-Allow-Credentials', 'true');
-    if ('OPTIONS' === req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-  });
+// app.use(function(req, res, next) {  
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header(
+//       'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
+//     );
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     if ('OPTIONS' === req.method) {
+//       res.sendStatus(200);
+//     }
+//     else {
+//       next();
+//     }
+//   });
   
 
 const corsOptions = {
--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 export const APT_URL = 'https://password-manager-git-master-tejaschaudhari192s-projects.vercel.app/'
-export const API_URL = 'https://password-manager-production-2a70.up.railway.app/api/'
+export const API_URL = 'https://password-manager-git-master-tejaschaudhari192s-projects.vercel.app/api/'
