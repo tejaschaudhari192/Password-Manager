f3ff5ad40447e1b04b92e4a08076a2259b65b948
Tejas Chaudhari <jaybalaji192@gmail.com>
2025-01-03 11:05:36 +0530

    navigate on home

--- frontend/src/Desktop/hooks/useGetData.jsx
+++ frontend/src/Desktop/hooks/useGetData.jsx
@@ -5,16 +5,22 @@ import { setPasswordItems, setStatus } from '../utils/passwordSlice';
 import { API_URL } from '../../../config';
 import { getPasswords } from '../services/api';
 import { useAuth } from '../context/AuthContext';
+import { Navigate, useNavigate } from 'react-router-dom';
 const useGetData = () => {
     const token = localStorage.getItem('token');
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const id = localStorage.getItem('id');
     async function bolav() {
         // console.log(id);
-        const result = await getPasswords(token, id);
-        const data = result.data;
-        // console.log(data);
-        dispatch(setPasswordItems(data))
+        await getPasswords(token, id).then((result) =>{
+            const data = result.data;
+            dispatch(setPasswordItems(data))
+        }).catch((error)=>{
+            alert(error)
+            if (error.status == 401)
+                navigate('/login')
+        });
     }
 
     useEffect(() => {
