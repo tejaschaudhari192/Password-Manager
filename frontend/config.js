1437589644658bd7b81f2869633ec2659d7346cd
divyeshdhole <divyeshlic2003@gmail.com>
2025-01-16 14:25:20 +0530

    fixed api

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 export const APT_URL = 'https://password-manager-git-master-tejaschaudhari192s-projects.vercel.app/'
-export const APl_URL = 'https://password-manager-v826.onrender.com/api/'
-export const API_URL = 'http://localhost:5000/api/'
+export const API_URL = 'https://password-manager-v826.onrender.com/api/'
+export const APl_URL = 'http://localhost:5000/api/'
